bc1433eda55316e4922449af8944507afcfbc49a
Grace Hartzell <gracehartzell@utexas.edu>
2018-09-30 16:38:08 -0500

    Hooked up very basic styling and pages

---
+++ server.js
@@ -0,0 +1,38 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+
+const environment = process.env.NODE_ENV || 'development';
+const portOptions = {development: 8000, test: 6000};
+const defaultPort = portOptions[environment];
+const port = process.env.PORT || defaultPort;
+
+const knexPath = path.join(__dirname, 'knexfile.js');
+const app = express();
+const knex = require('./db/knex');
+
+app.use(methodOverride('_method'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join('public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+app.disable('x-powered-by');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use((req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on PORT ${port}`);
+});
+
+module.exports = app;
